bde8918c93f7ef3a4c4b79750b623f1ab52baea6
maximiliano02 <maxo2037@gmail.com>
2018-02-24 12:24:09 +0100

    VolvoCS_Unity
    Projet Unity AR, avec gamification

---
+++ VolvoCS_Unity/Assets/VolvoS/Scripts/Business/Achievment.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TeamAVA.VolvoS.Gamification.Business {
+
+	public class Achievment : Rewardable {
+
+		/// <summary> All tasks need to be completed to complete achievment</summary>
+		[SerializeField] private bool m_Completed;
+		[SerializeField] private List<Task> m_Tasks;
+
+		public Achievment (){
+			m_Tasks = new List<Task> ();
+		}
+
+		public List<Task> Tasks {
+			get {
+				return this.m_Tasks;
+			}
+		}
+
+	}
+}
